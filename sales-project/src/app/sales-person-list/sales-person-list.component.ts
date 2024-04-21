94c291dc12867c1648604968dc5ea89349a23dc3
Vuong Gia Hy <hyvuonggia@gmail.com>
2024-04-21 09:17:33 +0700

    sales-project

---
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrl: './sales-person-list.component.css',
+})
+export class SalesPersonListComponent {
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson('Anup', 'Kumar', 'anup.kumar@gmail.com', 50000),
+    new SalesPerson('John', 'Doe', 'john.doe@gmail.com', 40000),
+    new SalesPerson('Gia Hy', 'Vuong', 'hyvuonggia@gmail.com', 30000),
+    new SalesPerson('Mai', 'Truong', 'mai.truong@gmail.com', 60000),
+  ];
+}
